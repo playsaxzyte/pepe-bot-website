910fffed758d58935ed863334eb1a0d71c97ef1d
playsaxzyte <playsaxzyte@gmail.com>
2026-05-20 23:01:30 +0530

    fixed vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import MusicPlayer from "./MusicPlayer";
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import menuVideo from './assets/Mainn.mp4'
@@ -10,6 +9,7 @@ import './App.css'
 
 function MenuScreen() {
   const navigate = useNavigate()
+
   return (
     <div id="menu-screen">
       <video src={menuVideo} autoPlay loop muted playsInline />
@@ -20,27 +20,41 @@ function MenuScreen() {
 
 function AnimatedRoutes() {
   const location = useLocation()
+
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={
-          <PageTransition><MenuScreen /></PageTransition>
-        } />
-        <Route path="/about" element={
-          <PageTransition variant="about"><AboutMe /></PageTransition>
-        } />
-        <Route path="/socials" element={
-          <PageTransition variant="socials"><Socials /></PageTransition>
-        } />
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <MenuScreen />
+            </PageTransition>
+          }
+        />
+
+        <Route
+          path="/about"
+          element={
+            <PageTransition variant="about">
+              <AboutMe />
+            </PageTransition>
+          }
+        />
+
+        <Route
+          path="/socials"
+          element={
+            <PageTransition variant="socials">
+              <Socials />
+            </PageTransition>
+          }
+        />
       </Routes>
     </AnimatePresence>
   )
 }
+
 export default function App() {
-return (
-  <>
-    <AnimatedRoutes />
-    <MusicPlayer />
-  </>
-);
+  return <AnimatedRoutes />
 }
